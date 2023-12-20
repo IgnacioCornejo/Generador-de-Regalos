83ab4707268ca38f40cc699329bd29b97eb0715a
Ignacio Cornejo <ignacio06cornejo@gmail.com>
2023-12-20 03:10:47 -0300

    Agregada funcionalidad en botones

--- app/components/button.jsx
+++ app/components/button.jsx
@@ -1,10 +1,20 @@
-
-const Button = ({btnBgColor,btnColor, btnMarginTop, btnText}) => {
-  return (
-    <>
-      <button className="rounded-full w-16 h-6" style={{backgroundColor: btnBgColor, width: "320px",height: "40px", color: btnColor, marginTop:btnMarginTop}}>{btnText}</button>
-    </>
-  )
-}
-
-export default Button
+const Button = ({ btnBgColor, btnColor, btnMarginTop, btnText, onClick }) => {
+    return (
+      <button
+        className="rounded-full w-16 h-6"
+        style={{
+          backgroundColor: btnBgColor,
+          width: '320px',
+          height: '40px',
+          color: btnColor,
+          marginTop: btnMarginTop
+        }}
+        onClick={onClick} // Pass onClick handler to the button element
+      >
+        {btnText}
+      </button>
+    );
+  };
+  
+  export default Button;
+  
--- app/page.js
+++ app/page.js
@@ -1,60 +1,104 @@
+'use client';
 
-import Dot from "./components/dots"
-import H1 from "./components/h1"
-import Button from "./components/button"
-import Paragraph from "./components/p"
- 
- 
+import { useState } from 'react';
+import Dot from './components/dots';
+import H1 from './components/h1';
+import Button from './components/button';
+import Paragraph from './components/p';
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1); // State to track the current page
+
+  const handleNextPage = () => {
+    setCurrentPage(prevPage => prevPage + 1); // Function to increment page number
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(prevPage => Math.max(prevPage - 1, 1)); // Function to decrement page number
+  };
+
   return (
     <>
-    <div className="flex flex-col items-center">
-     <Dot
-     backgroundColor1= "#6750A4"
-     backgroundColor2= "#D9D9D9"
-     backgroundColor3= "#D9D9D9" 
-     />
-     <img className="w-52 h-52 mb-4" src="/imagenScreen1.svg"></img>
-     <H1
-     h1Text="Vas a poder elegír para quién es"/>
-     <H1
-     h1Text="el regalo"/>
-     <Paragraph
-     pText="Tene en cuanta que podes selecciónarlo"/>
-     <Paragraph
-     pText="las veces que quieras "/>
-     <Button
-     btnBgColor="#6750A4"
-     btnColor="white"
-     btnMarginTop="100px"
-     btnText="Siguiente"/>
-     <Dot
-     backgroundColor1= "#6750A4"
-     backgroundColor2= "#D9D9D9"
-     backgroundColor3= "#D9D9D9" 
-     />
-     <img className="w-52 h-52 mb-4" src="/imagenScreen1.svg"></img>
-     <H1
-     h1Text="Vas a poder elegír para quién es"/>
-     <H1
-     h1Text="el regalo"/>
-     <Paragraph
-     pText="Tene en cuanta que podes selecciónarlo"/>
-     <Paragraph
-     pText="las veces que quieras "/>
-     <Button
-     btnBgColor="#6750A4"
-     btnColor="white"
-     btnMarginTop="100px"
-     btnText="Siguiente"/>
-     <Button
-     btnBgColor="#79747E"
-     btnColor="6750A4"
-     btnMarginTop="10px"
-     btnText="Atras"/>
+      <div className="flex flex-col items-center">
+        {currentPage === 1 && (
+          <>
+            <Dot
+              backgroundColor1="#6750A4"
+              backgroundColor2="#D9D9D9"
+              backgroundColor3="#D9D9D9"
+            />
+            <img className="w-52 h-52 mb-4" src="/imagenScreen1.svg" alt="Screen 1"></img>
+            <H1 h1Text="Vas a poder elegír para quién es" />
+            <H1 h1Text="el regalo" />
+            <Paragraph pText="Tene en cuenta que podes seleccionarlo" />
+            <Paragraph pText="las veces que quieras" />
+            <Button
+              btnBgColor="#6750A4"
+              btnColor="white"
+              btnMarginTop="100px"
+              btnText="Siguiente"
+              onClick={handleNextPage}
+            />
+          </>
+        )}
 
-     </div>
+        {currentPage === 2 && (
+          <>
+            <Dot
+              backgroundColor1="#6750A4"
+              backgroundColor2="#D9D9D9"
+              backgroundColor3="#D9D9D9"
+            />
+            <img className="w-52 h-52 mb-4" src="/imagenScreen2.svg" alt="Screen 2"></img>
+            <H1 h1Text="Vas a poder elegír para quién es" />
+            <H1 h1Text="el regalo" />
+            <Paragraph pText="Tene en cuenta que podes seleccionarlo" />
+            <Paragraph pText="las veces que quieras" />
+            <Button
+              btnBgColor="#6750A4"
+              btnColor="white"
+              btnMarginTop="100px"
+              btnText="Siguiente"
+              onClick={handleNextPage}
+            />
+            <Button
+              btnBgColor="#79747E"
+              btnColor="6750A4"
+              btnMarginTop="10px"
+              btnText="Atras"
+              onClick={handlePrevPage}
+            />
+          </>
+        )}
+        {currentPage === 3 && (
+          <>
+            <Dot
+              backgroundColor1="#6750A4"
+              backgroundColor2="#D9D9D9"
+              backgroundColor3="#D9D9D9"
+            />
+            <img className="w-52 h-52 mb-4" src="/imagenScreen3.svg" alt="Screen 2"></img>
+            <H1 h1Text="Vas a poder elegír para quién es" />
+            <H1 h1Text="el regalo" />
+            <Paragraph pText="Tene en cuenta que podes seleccionarlo" />
+            <Paragraph pText="las veces que quieras" />
+            <Button
+              btnBgColor="#6750A4"
+              btnColor="white"
+              btnMarginTop="100px"
+              btnText="Siguiente"
+              onClick={handleNextPage}
+            />
+            <Button
+              btnBgColor="#79747E"
+              btnColor="6750A4"
+              btnMarginTop="10px"
+              btnText="Atras"
+              onClick={handlePrevPage}
+            />
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
