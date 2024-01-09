c197916ce25f67470efe11ce0109c92f2437a7d2
Ignacio Cornejo <ignacio06cornejo@gmail.com>
2024-01-08 23:54:51 -0300

    prueba 2 exp default

--- app/giftsDatabase.js
+++ app/giftsDatabase.js
@@ -1,11 +1,12 @@
-'use client';
 
-export const gifts = [
+
+  const gifts = [
     { name: "Pelota", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 0, max: 5 }, gustos: ["Juegos"] },
     { name: "Bate de Beisbol", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 6, max: 12 }, gustos: ["Juegos"] },
     { name: "prueba 1", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 19, max: 120 }, gustos: ["Juegos"] },
     { name: "prueba 2", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 6, max: 12 }, gustos: ["Musica","Tecnologia"] }
   ];
+  export default gifts;
   
   localStorage.setItem('giftsData', JSON.stringify(gifts));
 
