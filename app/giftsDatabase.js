94ba92f46e4a70eb538638ddb17f7a20e6617b60
Ignacio Cornejo <ignacio06cornejo@gmail.com>
2024-01-15 00:21:12 -0300

    regalos agregados

--- app/giftsDatabase.js
+++ app/giftsDatabase.js
@@ -1,21 +1,29 @@
 
 
 const gifts = [
-    { name: "instrumento musical para niños", relacion: ["Familiar"], edad: { min: 0, max: 5 }, gustos: ["Musica"] },
-    { name: "Pelota", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 0, max: 5 }, gustos: ["Juegos"] },
+    { name: "Instrumento musical para niños", relacion: ["Familiar"], edad: { min: 0, max: 5 }, gustos: ["Musica"] },
+    { name: "Tablet educativa", relacion: ["Familiar"], edad: { min: 0, max: 5 }, gustos: ["Tecnologia"] },
+    { name: "Bateria", relacion: ["Familiar", "Amigo","Pareja"], edad: { min: 6, max: 19 }, gustos: ["Musica"] },
     { name: "Pelota", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 0, max: 5 }, gustos: ["Juegos"] },
     { name: "Bate de Beisbol", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 6, max: 12 }, gustos: ["Juegos"] },
-    { name: "prueba 1", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 19, max: 19 }, gustos: ["Juegos"] },
-    { name: "prueba 2", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 6, max: 12 }, gustos: ["Musica", "Tecnologia"] },
     { name: "Oso de peluche", relacion: ["Pareja"], edad: { min: 13, max: 18 }, gustos: ["Juegos"] },
-    { name: "Bloques de juguete", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 0, max: 5 }, gustos: ["Juegos"] },
+    { name: "Bloques de juguete", relacion: ["Familiar", "Amigo"], edad: { min: 0, max: 5 }, gustos: ["Juegos"] },
     { name: "Bicicleta", relacion: ["Familiar"], edad: { min: 6, max: 12 }, gustos: ["Juegos"] },
     { name: "Guitarra", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 6, max: 12 }, gustos: ["Musica"] },
     { name: "Copa de vino", relacion: ["Familiar", "Amigo", "Compañero Laboral"], edad: { min: 19, max: 19 }, gustos: ["Cocina"] },
     { name: "Taza", relacion: ["Familiar", "Pareja", "Amigo", "Compañero Laboral"], edad: { min: 12, max: 19 }, gustos: ["Cocina"] },
     { name: "Tabla de madera para cortar", relacion: ["Familiar", "Amigo", "Compañero Laboral"], edad: { min: 12, max: 19 }, gustos: ["Cocina"] },
     { name: "Portaretrato digital", relacion: ["Familiar", "Amigo", "Compañero Laboral"], edad: { min: 19, max: 19 }, gustos: ["Tecnologia", "Arte"] },
-    { name: "Auriculares", relacion: ["Familiar", "Amigo", "Compañero Laboral"], edad: { min: 19, max: 19 }, gustos: ["Tecnologia", "Musica"] },
+    { name: "Auriculares", relacion: ["Familiar", "Amigo", "Compañero Laboral"], edad: { min: 13, max: 19 }, gustos: ["Tecnologia", "Musica"] },
+    { name: "Consola de videojuegos", relacion: ["Familiar","Amigo"], edad: { min: 6, max: 19 }, gustos: ["Tecnologia", "Juegos"] },
+    { name: "Laptop", relacion: ["Familiar","Amigo","Compañero Laboral"], edad: { min: 13, max: 19 }, gustos: ["Tecnologia"] },
+    { name: "Celular", relacion: ["Familiar","Amigo","Compañero Laboral"], edad: { min: 13, max: 19 }, gustos: ["Tecnologia"] },
+    { name: "Valija", relacion: ["Familiar","Amigo","Compañero Laboral"], edad: { min: 13, max: 19 }, gustos: ["Viajes"] },
+    { name: "Mochila", relacion: ["Familiar","Amigo","Compañero Laboral"], edad: { min: 13, max: 19 }, gustos: ["Viajes"] },
+    { name: "Bolso", relacion: ["Familiar","Amigo","Compañero Laboral","Pareja"], edad: { min: 6, max: 19 }, gustos: ["Deportes","Viajes"] },
+    { name: "Rompecabezas", relacion: ["Familiar","Amigo","Compañero Laboral","Pareja"], edad: { min: 0, max: 19 }, gustos: ["Juegos"] }
+
+    
 ];
 export default gifts;
 
